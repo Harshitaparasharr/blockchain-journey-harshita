7e4f13d68050190431a27c842b25ff45f760c2f7
harshita parashar <harshitaparashar028@gmail.com>
2025-07-19 22:24:19 +0530

    Add files via upload

---
+++ Day2_first_contract_19july/basic.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract MyBasic {
+    string public myname = "harshita";
+    uint public myage = 20;
+    bool public isqueeen = true;
+    address public mywallet = msg.sender;
+}
